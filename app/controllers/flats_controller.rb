1cd5aa05cfcb44dd512e43b16078b55fa8425c31
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-14 20:10:44 -0300

    Adding CSS to index page - Show Page also Created

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,8 +3,27 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def show
+    @flats = Flat.find(params[:id])
+  end
+
   def new
     @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
   end
 
 
