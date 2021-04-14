6447c496aa6b7cd40a2b4c6c811ff1715d2c3ad7
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-14 18:34:16 -0300

    Using db:seed to populate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Spacious Flat Paris',
+  address: '111  place de Miremont',
+  description: 'Two double bedrooms, open plan living area, close to main restaurants',
+  price_per_night: 95,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Small flat Milan',
+  address: '56 Via Giotto',
+  description: 'Fell as a real italian in this cozy. one bedrooms, open plan living area',
+  price_per_night: 85,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Special flat London ',
+  address: '35  Wood Lane',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
