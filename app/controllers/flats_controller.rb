7935ef07c8546ce6abf47ed30730306aa652eea6
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-15 23:12:57 -0300

    Adding update action on Flat controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -24,11 +24,18 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
   end
 
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
   private
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night, :image)
   end
-
-
 end
